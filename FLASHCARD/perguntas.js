44dfb39faa45a044428c0dc7a2c1bfc0e49948f4
Gabyolivee <gaaboliveer09@gmail.com>
2024-10-08 09:14:06 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'política',
+    'quem é o presidente do brasil?',
+    'lula'
+)
+
+criaCartao(
+    'Geografia',
+    'qual a capital dos estados unidos?',
+    'washington'
+)
+
+criaCartao(
+    'ATUALIDADES',
+    'quantas namoradas o venancio tem?',
+    'nenhuma'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em inglÊs?',
+    'bom dia  em inglÊs é good morning'
+)
